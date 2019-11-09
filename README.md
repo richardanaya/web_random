b7f3617d11d80e52e0295afafb82ead8b1c2d242
richard <richard>
2019-11-08 23:36:38 -0800

    first pass

---
+++ README.md
@@ -0,0 +1,3 @@
+# web_random
+
+A [`js_ffi`](https://www.github.com/richardanaya/js_ffi) library for generating random numbers in browser.
---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+#![no_std]
+use js_ffi::*;
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
+
+pub struct Random {
+    rng:StdRng,
+}
+
+impl Default for Random {
+    fn default() -> Self {
+        let random = register("Math.random");
+        let rng:StdRng = SeedableRng::from_seed([
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+            (255.0*call_0(UNDEFINED,random)) as u8,
+        ]);
+        Random {
+            rng:rng,
+        }
+    }
+}
+
+impl Random {
+    pub fn gen<T>(&mut self) -> T where rand::distributions::Standard:rand::distributions::Distribution<T> {
+        self.rng.gen::<T>()
+    }
+}
